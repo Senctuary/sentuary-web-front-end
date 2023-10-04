41c056325f07f5a16a4f8421b59cdcb07adca7e6
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-10-04 17:02:22 +0700

    Thong - add comment for FirstPage Checkout

--- src/components/customer/checkout/FirstPage.js
+++ src/components/customer/checkout/FirstPage.js
@@ -2,11 +2,26 @@ import React from "react";
 import { Outlet } from "react-router";
 import SubHeader from "../../common/SubHeader";
 
+const cartItems = localStorage.getItem("cartItems");
+//Get the cart items from LS or API.
+// Then GET item information from API View a product detail
+
+// {
+//   "cartItems": [
+//     {
+//       "id": "123e",
+//       "name": "Nike Slim Shirt",
+//       "quantity": 10,
+// }
+// ]
+// }
+
 const FirstPage = () => {
   return (
     <div className="firstpage-container">
-      <SubHeader title="Add info" progressBar="inline-block"/>
-      <h1>FirstPage</h1>
+      <SubHeader title="Add info" progressBar="inline-block" />
+      <h2>Order Summary</h2>
+
       <Outlet />
     </div>
   );
