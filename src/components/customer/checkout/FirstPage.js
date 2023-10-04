3cc0530d6fea11ecd4eb9e5e4aeb3f8d519a6f3b
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-10-04 16:53:04 +0700

    Thong - complete subHeader component

--- src/components/customer/checkout/FirstPage.js
+++ src/components/customer/checkout/FirstPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router";
+import SubHeader from "../../common/SubHeader";
 
 const FirstPage = () => {
   return (
-    <div>
+    <div className="firstpage-container">
+      <SubHeader title="Add info" progressBar="inline-block"/>
       <h1>FirstPage</h1>
       <Outlet />
     </div>
