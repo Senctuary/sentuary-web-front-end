e22feec480a151d690d804f4f58721bbd40a1cf4
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-10-07 11:57:12 +0700

    Thong - get products in customize, install Swiper

--- src/components/screens/CustomizeScreen.js
+++ src/components/screens/CustomizeScreen.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Swiper from 'swiper';
+
+// const API_DOMAIN = process.env.REACT_APP_API_DOMAIN;
+const API_DOMAIN = "https://6520dfdb906e276284c4c0db.mockapi.io";
 
 const CustomizeScreen = () => {
-  return (
-    <div>CustomizeScreen</div>
-  )
-}
+  const [plants, setPlants] = useState([]);
+  const [vases, setVases] = useState([]);
 
-export default CustomizeScreen
+  let getPlants = () => {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`${API_DOMAIN}/plants`)
+        .then((response) => {
+          if (response) {
+            setPlants(response.data);
+            console.log(plants);
+          }
+        })
+        .catch((error) => {
+          throw error;
+        });
+    });
+  };
+
+  let getVases = () => {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`${API_DOMAIN}/plants`)
+        .then((response) => {
+          if (response) {
+            setVases(response.data);
+            console.log(vases);
+          }
+        })
+        .catch((error) => {
+          throw error;
+        });
+    });
+  };
+
+  useEffect(() => {
+    getPlants();
+    getVases();
+  }, []);
+
+  return <div>CustomizeScreen</div>;
+};
+
+export default CustomizeScreen;
