1c7a6967608411075d1b33f80e4ea6ac12dd0478
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-10-09 20:29:22 +0700

    Thong - update chatGPT command

--- src/components/customer/ChatGPT.js
+++ src/components/customer/ChatGPT.js
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 const ChatGPT = () => {
   const [response, setResponse] = useState("");
   const [loading, setLoading] = useState(false);
+  const [showForm, setShowForm] = useState(true);
 
   const phongCachOptions = [
     "hiện đại",
@@ -27,7 +28,7 @@ const ChatGPT = () => {
     const apiEndpoint = "https://ai.fakeopen.com/v1/chat/completions";
     const apiKey = "pk-this-is-a-real-free-pool-token-for-everyone";
 
-    let message = `Hãy đề xuất ngắn gọn cho tôi 3 loại sen đá, 3 loại chậu đất nung phù hợp với tôi. Tôi ${values.tuoi} tuổi, giới tính ${values.gioiTinh}, thích phong cách ${values.phongCach} và tôi muốn ${values.message}`;
+    let message = `Hãy đề xuất (dưới 250 từ) cho tôi 3 loại sen đá, 3 loại chậu đất nung phù hợp với tôi (trình bày với định dạng HTML chỉ có phần bên trong <body>). Tôi ${values.tuoi} tuổi, giới tính ${values.gioiTinh}, thích phong cách ${values.phongCach} và tôi muốn ${values.message}.`;
     console.log(message);
 
     fetch(apiEndpoint, {
@@ -50,6 +51,7 @@ const ChatGPT = () => {
       .then((response) => response.json())
       .then((data) => {
         setLoading(false);
+        setShowForm(false);
         console.log(data);
         setResponse(data.choices[0].message.content);
       })
@@ -60,43 +62,67 @@ const ChatGPT = () => {
 
   return (
     <div>
-      <Formik initialValues={{ initialValues }} onSubmit={handleSubmit}>
-        <Form>
-          <div>
-            <label>
-              Tuổi:
-              <Field type="number" name="tuoi" />
-            </label>
+      {showForm === true ? (
+        <>
+          <Formik initialValues={{ initialValues }} onSubmit={handleSubmit}>
+            <Form>
+              <div>
+                <label>
+                  Tuổi:
+                  <Field type="number" name="tuoi" />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Giới tính:
+                  <Field type="text" name="gioiTinh" />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Phong cách:
+                  {phongCachOptions.map((option) => (
+                    <div key={option}>
+                      <label>
+                        <Field
+                          type="checkbox"
+                          name="phongCach"
+                          value={option}
+                        />
+                        {option}
+                      </label>
+                    </div>
+                  ))}
+                </label>
+              </div>
+              <div>
+                <label>
+                  Mong muốn của bạn:
+                  <Field type="text" name="message" />
+                </label>
+              </div>
+              <Button
+                label="Gửi"
+                type="submit"
+                value="Submit"
+                loading={loading}
+              />
+            </Form>
+          </Formik>
+        </>
+      ) : (
+        <>
+          <div className="result-container">
+            <div className="result-message" dangerouslySetInnerHTML={{__html: response}}></div>
+            <Button
+              label="Thử lại"
+              onClick={() => {
+                setShowForm(true);
+              }}
+            />
           </div>
-          <div>
-            <label>
-              Giới tính:
-              <Field type="text" name="gioiTinh" />
-            </label>
-          </div>
-          <div>
-            <label>
-              Phong cách:
-              {phongCachOptions.map((option) => (
-                <div key={option}>
-                  <label>
-                    <Field type="checkbox" name="phongCach" value={option} />
-                    {option}
-                  </label>
-                </div>
-              ))}
-            </label>
-          </div>
-          <div>
-            <label>
-              Mong muốn của bạn:
-              <Field type="text" name="message" />
-            </label>
-          </div>
-          <Button label="Gửi" type="submit" value="Submit" loading={loading} />
-        </Form>
-      </Formik>
-      <div className="result-message">{response}</div>
+        </>
+      )}
     </div>
   );
 };
