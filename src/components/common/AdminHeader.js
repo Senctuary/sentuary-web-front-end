14c5c5e1d3c2360f7e461f83f63a4c951b354ecc
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2023-10-20 04:48:32 +0700

    Anh - Handle Admin Logout

--- src/components/common/AdminHeader.js
+++ src/components/common/AdminHeader.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./styles/SubHeader.css";
+import { useNavigate } from "react-router-dom";
+import { Button } from "primereact/button";
 
 const AdminHeader = (props) => {
+
+  const nav = useNavigate();
+
+  const handleLogout = () => {  
+    localStorage.removeItem("jwtToken");
+    nav("/login");
+  }
+
   return (
-    <div className="subHeader-container" style={{width: "100%"}}>
+    <div className="subHeader-container" style={{width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center"}}>
       <div className="title">{props.title}</div>
+      <Button className="logout-button" onClick={handleLogout}>Logout</Button>
     </div>
   )
 }
